50024f0eb561cea2fb59e57600a3340d63b258f4
isae <isaev.ilya.1994@gmail.com>
2014-11-24 15:58:10 +0300

    made a simple annotation-driven server

---
+++ src/ru/ifmo/ctddev/web/controller/ShitController.java
@@ -0,0 +1,29 @@
+package ru.ifmo.ctddev.web.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by root on 11/22/14.
+ */
+@Controller
+@RequestMapping("/shit")
+public class ShitController {
+
+    protected final Log logger = LogFactory.getLog(getClass());
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView printHello(HttpServletRequest request, HttpServletResponse response) {
+        ModelAndView mv = new ModelAndView("/hello.jsp");
+        logger.warn("Returning hello view");
+        mv.addObject("message", "Hello Spring MVC Framework!");
+        return mv;
+    }
+}
---
+++ web/jsp/index.jsp
@@ -0,0 +1,18 @@
+<%--
+ Copyright 2004-2005 Sun Microsystems, Inc.  All rights reserved.
+ Use is subject to license terms.
+--%>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="s" uri="http://www.springframework.org/tags" %>
+
+<html>
+<head><title>Hello</title></head>
+<body bgcolor="white">
+<img src="<s:url value="/images/duke.gif"/>"/>
+
+<h1>Hello, SERVER IS RUNNING</h1>
+
+</body>
+</html>
